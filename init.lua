7f6facf372e4e226e70ec9d825bc06d1a158d0dc
Jeremy Van de Wyngaert <jeremy.vandewyngaert@exoscale.com>
2024-02-26 11:06:54 +0100

    add catppuccin

--- init.lua
+++ init.lua
@@ -31,6 +31,7 @@ require('packer').startup(function()
   use 'sainnhe/everforest'
   use 'tomasiser/vim-code-dark'
   use 'dikiaap/minimalist'
+  use { "catppuccin/nvim", as = "catppuccin" }
 
   use {
     'nvim-lualine/lualine.nvim',
@@ -111,23 +112,90 @@ vim.wo.signcolumn = 'yes'
 -- vim.o.termguicolors = true
 -- vim.g.onedark_terminal_italics = 2
 -- vim.cmd [[colorscheme onedark]]
-require('onedark').setup {
-  style = 'darker',
-  colors = {
-    bg0 = "#181818",
-  },
-  transparent = false, -- hide/show bg
-  -- Lualine options --
-  lualine = {
-    transparent = false, -- lualine center bar transparency
-  },
-}
-require('onedark').load()
+-- require('onedark').setup {
+--   style = 'darker',
+--   colors = {
+--     bg0 = "#181818",
+--   },
+--   transparent = false, -- hide/show bg
+--   -- Lualine options --
+--   lualine = {
+--     transparent = false, -- lualine center bar transparency
+--   },
+-- }
+-- require('onedark').load()
+
+-- catppuccin
+-- require("catppuccin").setup({
+--   flavour = "mocha", -- latte, frappe, macchiato, mocha
+--   background = { -- :h background
+--     light = "latte",
+--     dark = "mocha",
+--   },
+--   transparent_background = false, -- disables setting the background color.
+--   show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+--   term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+--   dim_inactive = {
+--     enabled = false, -- dims the background color of inactive window
+--     shade = "dark",
+--     percentage = 0.15, -- percentage of the shade to apply to the inactive window
+--   },
+--   no_italic = false, -- Force no italic
+--   no_bold = false, -- Force no bold
+--   no_underline = false, -- Force no underline
+--   styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+--     comments = { "italic" }, -- Change the style of comments
+--     conditionals = { "italic" },
+--     loops = {},
+--     functions = {},
+--     keywords = {},
+--     strings = {},
+--     variables = {},
+--     numbers = {},
+--     booleans = {},
+--     properties = {},
+--     types = {},
+--     operators = {},
+--   },
+--   color_overrides = {},
+--   custom_highlights = {},
+--   integrations = {
+--     cmp = true,
+--     gitsigns = true,
+--     nvimtree = true,
+--     treesitter = true,
+--     notify = false,
+--     mini = false,
+--     native_lsp = {
+--       enabled = true,
+--       virtual_text = {
+--         errors = { "italic" },
+--         hints = { "italic" },
+--         warnings = { "italic" },
+--         information = { "italic" },
+--       },
+--       underlines = {
+--         errors = { "underline" },
+--         hints = { "underline" },
+--         warnings = { "underline" },
+--         information = { "underline" },
+--       },
+--       inlay_hints = {
+--         background = true,
+--       },
+--     },
+--   },
+-- })
+require("catppuccin").setup({
+  flavour = "mocha",
+})
+-- require("catppuccin").load()
+
+vim.cmd.colorscheme "catppuccin"
 
 require('lualine').setup {
   options = {
-    theme = 'onedark'
-    -- ... your lualine config
+    theme = 'catppuccin'
   }
 }
 
