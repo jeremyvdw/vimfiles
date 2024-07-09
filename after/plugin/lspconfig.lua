e3cc975dd1e4aab9ace9200878d84bb3671b93dd
Jeremy Van de Wyngaert <jeremy.vandewyngaert@exoscale.com>
2024-07-09 09:18:13 +0200

    setup lua_ls

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -56,3 +56,31 @@ require('lspconfig')['jedi_language_server'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
+require('lspconfig')['lua_ls'].setup{
+  on_attach = on_attach,
+  flags = lsp_flags,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  }
+}
